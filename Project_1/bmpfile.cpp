ff4d2391c5a019e1278e747603df43900a20f521
John Ordoyo <gtom_1984@hotmail.ca>
2021-07-05 07:44:04 -0700

    More testing with autolevel

--- Project_1/bmpfile.cpp
+++ Project_1/bmpfile.cpp
@@ -11,26 +11,46 @@ BMPFile::BMPFile(QString fileLocation)
     }
 }
 
-QImage BMPFile::getAutoLevel() {
-    QImage newImage = imageFile.convertToFormat(QImage::Format_RGB32);
-    QRgb * bits = (QRgb*)newImage.constBits();
-    int size = newImage.width() * newImage.height();
-    for(int i = 0; i <= size; i++) {
-        std::cout << "i = " << i << std::endl;
-        std::cout << "Color R = "
-                << qRed(bits[size + i]) << ", G = "
-                << qGreen(bits[size + i]) << ", B = "
-                << qBlue(bits[size + i]) << std::endl;
-    }
-    std::cout <<" Finished!" << std::endl;
 
-
-
-    return QImage();
+int getHistEqualVal(int numPixels, int totalNumPixels, int * prevCdf) {
+    int pdf = numPixels / totalNumPixels;
+    *prevCdf = *prevCdf + pdf;
+    return *prevCdf * 255;
 }
 
-struct colorIntensityInfo {
-    int numPixels;
-    int intensity;
-    int newIntensity;
-};
+QImage BMPFile::getAutoLevel() {
+    QImage newImage = imageFile.convertToFormat(QImage::Format_RGB32);
+    QRgb * bits = (QRgb*)imageFile.constBits();
+
+    int size = newImage.width() * newImage.height();
+    int imageRgbs [3][256];
+
+    std::cout << "getting old image bits" << std::endl;
+    for(int i = size; i < size; i++) {
+        imageRgbs[0][qRed(bits[i])]++;
+        imageRgbs[1][qGreen(bits[i])]++;
+        imageRgbs[2][qBlue(bits[i])]++;
+    }
+
+    std::cout << "making new image" << std::endl;
+    QRgb * newBits = (QRgb*)newImage.bits();
+    int rCdf = 0, gCdf = 0, bCdf = 0;
+    for(int j = 0; j < size; j++) {
+
+        newBits[j] = QColor(
+                    getHistEqualVal(imageRgbs[0][qRed(newBits[j])], size, &rCdf),
+                    getHistEqualVal(imageRgbs[1][qGreen(newBits[j])], size, &gCdf),
+                    getHistEqualVal(imageRgbs[2][qBlue(newBits[j])], size, &bCdf)
+                    ).rgba();
+    }
+
+    if(newImage.save("/home/NewImage") == true) {
+        std::cout << "finished image manipulation" << std::endl;
+    }
+    else {
+        std::cout << "unable to save new image" << std::endl;
+    }
+}
+
+
+
